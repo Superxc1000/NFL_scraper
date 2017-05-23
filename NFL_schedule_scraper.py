d52d7785b396ca056a6b752cc9dc9deccc1a14fd
Greg <superxc1000@gmail.com>
2017-05-23 09:09:49 -0500

    Added Comments

--- NFL_schedule_scraper.py
+++ NFL_schedule_scraper.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
+
+# Team slugs manually taken off of www.pro-football-reference.com
 team_slug = ['nwe', 'mia','buf', 'nyj', 'pit', 'rav', 'cin', 'cle', 'htx', \
 	'oti', 'clt', 'jax', 'kan', 'rai', 'den', 'sdg', 'dal', 'nyg', 'was', 'phi',\
 	'gnb', 'det', 'min', 'chi', 'atl', 'tam', 'nor', 'car', 'sea', 'crd', 'ram', 'sfo']
@@ -12,12 +14,13 @@ for slug in team_slug:
 	response = requests.get("http://www.pro-football-reference.com/teams/"+ slug +"/2016_games.htm")
 	soup = BeautifulSoup(response.content, 'lxml')
 	lister = []
-	for row in soup.find('tbody').find_all('tr')[:17]:
+	for row in soup.find('tbody').find_all('tr')[:17]: #Using "17" limits the scrape to regular season games
 		try:
 			boxscore = row.find_all('td')[3].find('a')['href']
-			lister.append(boxscore[11:23])
+			lister.append(boxscore[11:23]) # This cuts the slug to the minimum
 		except:
 			continue
 	final_list += lister
 
+# Uses 'set' to remove duplicates and saves the file to csv
 pd.DataFrame(list(set(final_list)), columns=['Games']).to_csv('NFL_2016_reg_schedule.csv')
